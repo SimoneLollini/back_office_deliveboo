4a518e942fa7ed326de7d00c792e5094eff1f1c2
Danielateresa <dany.t_87@hotmail.it>
2023-02-03 17:52:14 +0100

    Add pivot table restaurants-types

---
+++ database/migrations/2023_02_03_164646_create_restaurat_type_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant_type', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('type')->cascadeOnDelete();
+
+            $table->primary(['restaurant_id', 'type_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant_type');
+    }
+};
