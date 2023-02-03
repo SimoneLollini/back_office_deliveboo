ecb840ea3a4fde885ebb46c409239eabeddfc2e2
Danielateresa <dany.t_87@hotmail.it>
2023-02-03 17:02:54 +0100

    Make types migration #4

--- app/Models/type.php
+++ app/Models/type.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class type extends Model
 {
     use HasFactory;
+
+     /**
+     * The roles that belong to the Technology
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function restaurants(): BelongsToMany
+    {
+        return $this->belongsToMany(Restaurant::class);
+    }
+
+
 }
