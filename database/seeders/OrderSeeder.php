c5b3244526eff2eceb2aaee7253f1be3763472b5
SimoneLollini <lollinisimone@gmail.com>
2023-02-04 21:30:49 +0100

    Define PlateSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -18,9 +18,9 @@ class OrderSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $newOrder = new Order();
-            $newOrder->price = $faker->randomFloat(2, 5, 60);;
-            $newOrder->phone = $faker->randomNumber(5, true);;
-            $newOrder->description = $faker->randomHtml(1, 1);;
+            $newOrder->price = $faker->randomFloat(2, 5, 60);
+            $newOrder->phone = $faker->randomNumber(5, true);
+            $newOrder->description = $faker->randomHtml(1, 1);
             $newOrder->full_name = $faker->name();
             $newOrder->adress = $faker->address();
             $newOrder->status = $faker->boolean();
--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Plate;
+use Faker\Generator as Faker;
 
 class PlateSeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class PlateSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $plates = ['Pasta', 'Pizza', 'Lasagne', 'Risotto', 'Polenta', 'Polpette', 'Arancine', 'Bistecca', 'Hamburger'];
+        foreach ($plates as $plate) {
+            $newPlate = new Plate();
+            $newPlate->name = $plate;
+            //$newPlate->description = $faker->randomHtml(1, 1);
+            //$newPlate->ingredients = $faker->randomHtml(0, 1);
+            //image
+            $newPlate->price = $faker->randomFloat(2, 5, 60);
+            $newPlate->visibility = $faker->boolean();
+            //$newPlate->type = $faker->randomHtml(0, 1);
+            //$newPlate->restaurant_id = $faker->numberBetween(0, 100);
+            $newPlate->save();
+        }
     }
 }
