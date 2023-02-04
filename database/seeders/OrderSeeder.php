81878cd9345e1d0b4efd1dedfd324e9b6b3178dc
SimoneLollini <lollinisimone@gmail.com>
2023-02-04 21:05:50 +0100

    Define OrderSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class OrderSeeder extends Seeder
 {
@@ -13,16 +14,16 @@ class OrderSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         for ($i = 0; $i < 10; $i++) {
             $newOrder = new Order();
-            $newOrder->price = 5;
-            $newOrder->phone = '3201239439';
-            $newOrder->description = 'lorem ipsum qualcosa';
-            $newOrder->full_name = 'lorem ipsum qualcosa';
-            $newOrder->adress = 'via del procinto 57, Milano';
-            $newOrder->status = true;
+            $newOrder->price = $faker->randomFloat(2, 5, 60);;
+            $newOrder->phone = $faker->randomNumber(5, true);;
+            $newOrder->description = $faker->randomHtml(1, 1);;
+            $newOrder->full_name = $faker->name();
+            $newOrder->adress = $faker->address();
+            $newOrder->status = $faker->boolean();
             $newOrder->save();
         }
     }
