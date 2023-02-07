c85399cb89849a41991894136acd33e1dabc5fdc
Salano24 <salernomarco48@email.com>
2023-02-07 14:04:01 +0100

    end orders index

--- resources/views/Admin/Orders/index.blade.php
+++ resources/views/Admin/Orders/index.blade.php
@@ -1,44 +1,82 @@
 @extends('layouts.app')
 @section('content')
-<h1 class="text-center pt-5 pb-5">I MIEI ORDINI</h1>
+<h1 class="text-center pt-5 pb-2 ">I MIEI ORDINI</h1>
 @if (session('message'))
 <div class="alert alert-success">
     {{ session('message') }}
 </div>
 @endif
-<div class="container">
-    <div class="table-responsive">
-        <table class="table table-striped align-middle">
-            <thead class="table-light text-center ">
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Numero </th>
-                    <th>Indirizzo</th>
-                    <th>Prezzo</th>
-                    <th>Descrizione</th>
-                    <th>Stato</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                @foreach($orders as $order)
-                <tr class="table-danger text-center">
-                    <td class="pt-4 pb-4">{{$order->id}}</td>
-                    <td>{{$order->full_name}}</td>
-                    <td>{{$order->phone}}</td>
-                    <td>{{$order->address}}</td>
-                    <td>{{$order->price}}</td>
-                    <td>{{$order->description}}</td>
-                    <td>{{$order->status}}</td>
-                </tr>
-                @endforeach
 
-            </tbody>
-            <tfoot>
+<div class="container_sm">
+    
 
-            </tfoot>
-        </table>
-    </div>
+        @foreach($orders as $order)
+        <div class="order text-center">
+        <span>ID: {{$order->id}} </span> <br>
+        <hr>
+        <span>Nome utente: {{$order->full_name}} </span> <br> 
+        <hr>
+        <span>Numero di telefono: {{$order->phone}} </span> <br>
+        <hr>
+        <span>Indirizzo: {{$order->address}} </span> <br>
+        <hr>
+        <span>Prezzo: €{{$order->price}} </span> <br>
+        <hr>
+        <span>Descrizione: {{$order->description}} </span> <br>
+        <hr>
+        <span>
+            Stato ordine: <br>
+          @if($order->status == 1)  
+          <span class="circle green">⬤</span>
+          @endif
+          @if($order->status == 0)  
+          <span class="circle red">⬤</span>
+          @endif
+        </span>
+        </div>
+        @endforeach
+    
 </div>
 
+
 @endsection
+<style lang="scss">
+
+h1{
+    color: #a43c28;
+}
+.container_sm{
+    display: flex;
+    flex-wrap:wrap;
+    width: 80%;
+    
+    margin: auto;
+   
+}
+    .order{
+        width: 32%;
+        background-color: #f6edda;
+        border-radius: 1rem;
+        color: #a43c28;
+        font-weight: 700;
+        padding: 1rem;
+        margin-bottom:0.85rem ;
+        margin-left: 0.40rem;
+        margin-right: 0.40rem;
+        
+        
+    }
+
+    .circle{
+        font-size: 2rem;
+    }
+
+    .green{
+        color: green;
+    }
+    .red{
+        color: red;
+    }
+
+
+</style>
