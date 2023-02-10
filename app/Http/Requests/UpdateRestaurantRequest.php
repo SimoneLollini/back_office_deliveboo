57c87295ccbe0e8b1c61080051b012d0e0c68689
SevenErol <lorenzo.maggi1998@gmail.com>
2023-02-10 18:39:44 +0100

    Order and plate fixed

--- app/Http/Requests/UpdateRestaurantRequest.php
+++ app/Http/Requests/UpdateRestaurantRequest.php
@@ -26,12 +26,35 @@ class UpdateRestaurantRequest extends FormRequest
     {
         return [
             'name' => 'required|max:100',
-            'type_id' => 'required',
+            'types' => 'required|exists:types,id',
             'phone' => 'required|min:10|max:12',
             'piva' => ['required', Rule::unique('restaurants')->ignore($this->restaurant->id), 'digits:11'],
             'address' => 'required',
             'user_id' => 'unique:restaurant',
             'restaurant_image' => 'required|image|max:512'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Attenzione! Inserisci un nome per il tuo ristorante.',
+            'name.max' => 'Hai superato il numero massimo di caratteri (100).',
+            'types.required' => 'Attenzione! Inserisci la tipologia del tuo ristorante.',
+            'phone.required' => 'Attenzione! Inserisci il numero di telefono del tuo ristorante.',
+            'phone.min' => 'Il numero inserito deve avere almeno 10 cifre.',
+            'phone.max' => 'Il numero inserito non deve avere più di 12 cifre.',
+            'piva.required' => 'Attenzione! Inserisci la partita IVA.',
+            'piva.unique' => 'Attenzione! Questa partita IVA è già stata utilizzata.',
+            'piva.digits' => 'Attenzione! La partita IVA deve essere formata da 11 cifre.',
+            'address.required' => 'Attenzione! Inserisci un indirizzo per il tuo ristorante.',
+            'restaurant_image.max' => 'L\'immagine è troppo pesante, utilizzane una al di sotto dei 512KB.',
+            'restaurant_image.required' => 'Attenzione! Inserisci un\'immagine.'
         ];
     }
 }
--- resources/views/Admin/Orders/index.blade.php
+++ resources/views/Admin/Orders/index.blade.php
@@ -1,50 +1,61 @@
 @extends('layouts.admin')
 @section('content')
-    <h1 class="text-center pt-5 pb-2 ">I MIEI ORDINI</h1>
+    <div class="container">
 
-    @if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
+        <h1 class="text-center pt-5 pb-2 ">I MIEI ORDINI</h1>
+
+        <hr class="new">
+
+        <div class="d-flex justify-content-center">
+
+            <div class=" text-center">{{ $collection->links() }}</div>
+
+        </div>
+    </div>
+
+    @if (!$collection->isEmpty())
+        <div class="container_sm">
+
+            @foreach ($collection as $order)
+                <div class="order text-center">
+                    <span>ID: {{ $order->first()->id }} </span> <br>
+                    <hr>
+                    <span>Nome utente: {{ $order->first()->full_name }} </span> <br>
+                    <hr>
+                    <span>Numero di telefono: {{ $order->first()->phone }} </span> <br>
+                    <hr>
+                    <span>Indirizzo: {{ $order->first()->address }} </span> <br>
+                    <hr>
+                    <span>Prezzo: €{{ $order->first()->price }} </span> <br>
+                    <hr>
+                    <span>Descrizione: {{ $order->first()->description }} </span> <br>
+                    <hr>
+                    <span>
+                        Stato ordine: <br>
+                        @if ($order->first()->status == 1)
+                            <span class="circle green">⬤</span>
+                        @endif
+                        @if ($order->first()->status == 0)
+                            <span class="circle red">⬤</span>
+                        @endif
+                    </span>
+                </div>
+            @endforeach
+
+        </div>
+    @else
+        <div class="container">
+
+            <div class="row align-items-center justify-content-center p-5 ms_order">
+
+                <h3 id="ms_title" class="text-dark-red col-6">Non ci sono ancora ordini effettuati</h3>
+
+                <img src="{{ asset('/img/deliveboo-logo.png') }}" alt="" class="img-fluid col-4">
+
+            </div>
+
         </div>
     @endif
-    <div class="d-flex justify-content-center">
-
-        <div class=" text-center">{{ $collection->links() }}</div>
-    </div>
-
-
-    <div class="container_sm">
-
-
-        @foreach ($collection as $order)
-            <div class="order text-center">
-                <span>ID: {{ $order->first()->id }} </span> <br>
-                <hr>
-                <span>Nome utente: {{ $order->first()->full_name }} </span> <br>
-                <hr>
-                <span>Numero di telefono: {{ $order->first()->phone }} </span> <br>
-                <hr>
-                <span>Indirizzo: {{ $order->first()->address }} </span> <br>
-                <hr>
-                <span>Prezzo: €{{ $order->first()->price }} </span> <br>
-                <hr>
-                <span>Descrizione: {{ $order->first()->description }} </span> <br>
-                <hr>
-                <span>
-                    Stato ordine: <br>
-                    @if ($order->first()->status == 1)
-                        <span class="circle green">⬤</span>
-                    @endif
-                    @if ($order->first()->status == 0)
-                        <span class="circle red">⬤</span>
-                    @endif
-                </span>
-            </div>
-        @endforeach
-
-
-
-    </div>
 @endsection
 <style lang="scss">
     h1 {
@@ -84,5 +95,15 @@
 
     .red {
         color: red;
+    }
+
+    hr.new {
+        border: 2px solid #A43C28;
+        opacity: 1;
+    }
+
+    .ms_order {
+        background-color: #f6edda;
+        border-radius: 5px;
     }
 </style>
