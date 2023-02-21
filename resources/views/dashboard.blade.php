7a1b40c1bd122b738bc5cfe7188582e8d4fd84ff
Danielateresa <dany.t_87@hotmail.it>
2023-02-21 16:38:03 +0100

    Edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,171 +1,178 @@
 @extends('layouts.admin')
 
 @section('content')
-    @if ($user_restaurant)
-        <div class="container px-3 py-2">
-            <h1 class="text-dark-red text-center">
-                <strong>Benvenuto nel pannello di amministrazione</strong>
-            </h1>
-            <hr class="new">
-        </div>
+@if ($user_restaurant)
+<div class="shadow-sm mx-0 py-3 ">
 
-        <div class="container align-items-center card p-5 rounded-4 border-0">
+    <h1 class="text_dark_red text-center py-2 fw-bold ps-3">Pannello di amministrazione<img class="dashboard_icon ms-3"
+            src="{{ asset('/img/dashboard_icon.png') }}" alt=""></h1>
+</div>
 
-            @include ('partials.message')
-            <div class="col-4 text-center">
-                <img src="{{ asset('storage/' . $user_restaurant->restaurant_image) }}"
-                    alt="{{ $user_restaurant->restaurant_image }}" class="img-fluid" style="height: 240px">
-            </div>
+<div class="container align-items-center card shadow p-5 rounded-4 border-0 mt-4">
 
-            <div class="col-8 d-flex flex-column text-dark-red">
+    @include ('partials.message')
+    <div class="col-4 text-center">
+        <img src="{{ asset('storage/' . $user_restaurant->restaurant_image) }}"
+            alt="{{ $user_restaurant->restaurant_image }}" class="img-fluid" style="height: 240px">
+    </div>
 
-                <h2 class="my-3"> Nome Ristorante: {{ $user_restaurant->name }} </h2>
+    <div class="col-8 d-flex flex-column text-dark-red">
 
-                <p> <strong>Tipologia cucina:</strong>
-                    @foreach ($user_restaurant->types as $index => $type)
-                        {{ $type->name }} |
-                    @endforeach
+        <h2 class="my-3"> Nome Ristorante: {{ $user_restaurant->name }} </h2>
+
+        <p> <strong>Tipologia cucina:</strong>
+            @foreach ($user_restaurant->types as $index => $type)
+            {{ $type->name }} |
+            @endforeach
 
 
-                <p> <strong>Numero di telefono:</strong> {{ $user_restaurant->phone }} </p>
+        <p> <strong>Numero di telefono:</strong> {{ $user_restaurant->phone }} </p>
 
-                <p> <strong>Partita iva del tuo ristorante:</strong> {{ $user_restaurant->piva }} </p>
+        <p> <strong>Partita iva del tuo ristorante:</strong> {{ $user_restaurant->piva }} </p>
 
-                <p> <strong>Il tuo indirizzo:</strong> {{ $user_restaurant->address }}</p>
+        <p> <strong>Il tuo indirizzo:</strong> {{ $user_restaurant->address }}</p>
 
-            </div>
+    </div>
 
-            <a href="{{ route('admin.restaurants.edit', $user_restaurant->id) }}" type="button"
-                class='btn btn_edit btn_fit ms-auto'>Modifica
-                dati ristorante</a>
-        </div>
-        </div>
-    @else
-        <div class="background-log-reg  100vh">
-            <div class="container px-3 px-sm-5 py-5 h-100">
-                <div class="row justify-content-start h-100">
-                    <div class="col-md-7 col-lg-6 h-100">
-                        <div class="card px-4 py-5 rounded-5 border-0 h-100">
-                            <div class=" pb-3 fs-2"><span class="text_dark_red fw-bold">Crea il tuo ristorante</span></div>
+    <a href="{{ route('admin.restaurants.edit', $user_restaurant->id) }}" type="button"
+        class='btn btn_edit btn_fit ms-auto'>Modifica
+        dati ristorante</a>
+</div>
+</div>
+@else
+<div class="background-log-reg  100vh">
+    <div class="container px-3 px-sm-5 py-5 h-100">
+        <div class="row justify-content-start h-100">
+            <div class="col-md-7 col-lg-6 h-100">
+                <div class="card px-4 py-5 rounded-5 border-0 h-100">
+                    <div class=" pb-3 fs-2"><span class="text_dark_red fw-bold">Crea il tuo ristorante</span></div>
 
-                            <div><span class="text_green fw-bold">Puoi registrare un solo ristorante per ogni account</span>
+                    <div><span class="text_green fw-bold">Puoi registrare un solo ristorante per ogni account</span>
+                    </div>
+
+                    @include('partials.error')
+                    <div class="card-body">
+                        <form action="{{ route('admin.restaurants.store') }}" method="post"
+                            class="d-flex flex-column justify-content-between gap-1" enctype="multipart/form-data">
+                            @csrf
+
+                            {{-- SEPARATORE --}}
+
+                            <div class="row">
+                                <label for="name" class="text-dark-red mb-2">Nome ristorante</label>
+                                <input type="text" name="name" id="name"
+                                    class="form-control @error('name') is-invalid @enderror lh-0"
+                                    placeholder="nome ristorante" value="{{ old('name') }}" required>
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
+
+                                @error('name')
+                                <small id="nameHlper" class="invalid-feedback">{{ $message }} </small>
+                                @enderror
+
                             </div>
 
-                            @include('partials.error')
-                            <div class="card-body">
-                                <form action="{{ route('admin.restaurants.store') }}" method="post"
-                                    class="d-flex flex-column justify-content-between gap-1" enctype="multipart/form-data">
-                                    @csrf
+                            {{-- SEPARATORE --}}
 
-                                    {{-- SEPARATORE --}}
-
-                                    <div class="row">
-                                        <label for="name" class="text-dark-red mb-2">Nome ristorante</label>
-                                        <input type="text" name="name" id="name"
-                                            class="form-control @error('name') is-invalid @enderror lh-0"
-                                            placeholder="nome ristorante" value="{{ old('name') }}" required>
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-
-                                        @error('name')
-                                            <small id="nameHlper" class="invalid-feedback">{{ $message }} </small>
-                                        @enderror
-
+                            <div class="row">
+                                <label for="type_id" class="text-dark-red mb-2">Tipologia cucina</label>
+                                <div class="solid-wrapper">
+                                    @foreach ($types as $type)
+                                    <div class="mb-1">
+                                        <input type="checkbox" name="types[]" value="{{ $type->id }}"
+                                            id="{{ $type->id }}">
+                                        <label for="types">{{ $type->name }}</label>
                                     </div>
+                                    @endforeach
+                                </div>
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
 
-                                    {{-- SEPARATORE --}}
+                                @error('types')
+                                <small id="typesHlper" class="invalid-feedback">{{ $message }} </small>
+                                @enderror
 
-                                    <div class="row">
-                                        <label for="type_id" class="text-dark-red mb-2">Tipologia cucina</label>
-                                        <div class="solid-wrapper">
-                                            @foreach ($types as $type)
-                                                <div class="mb-1">
-                                                    <input type="checkbox" name="types[]" value="{{ $type->id }}"
-                                                        id="{{ $type->id }}">
-                                                    <label for="types">{{ $type->name }}</label>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-
-                                        @error('types')
-                                            <small id="typesHlper" class="invalid-feedback">{{ $message }} </small>
-                                        @enderror
-
-                                    </div>
-
-                                    {{-- SEPARATORE --}}
-
-                                    <div class="row">
-                                        <label for="phone" class="text-dark-red mb-2">Numero di telefono</label>
-                                        <input type="text" name="phone" id="phone"
-                                            class="form-control @error('phone') is-invalid @enderror"
-                                            placeholder="Aggiungi numero di telefono" value="{{ old('phone') }}" required
-                                            minlength="9" maxlength="12">
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-
-                                        @error('phone')
-                                            <small id="phoneHlper" class="invalid-feedback">{{ $message }} </small>
-                                        @enderror
-
-                                    </div>
-
-                                    {{-- SEPARATORE --}}
-
-                                    <div class="row">
-                                        <label for="piva" class="text-dark-red mb-2">P.IVA</label>
-                                        <input type="text" name="piva" id="piva"
-                                            class="form-control @error('piva') is-invalid @enderror"
-                                            placeholder="Aggiungi piva" value="{{ old('piva') }}" required minlength="11"
-                                            maxlength="11">
-
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-                                        @error('piva')
-                                            <small id="pivaHlper" class="invalid-feedback">{{ $message }} </small>
-                                        @enderror
-
-                                    </div>
-
-                                    {{-- SEPARATORE --}}
-
-                                    <div class="row">
-                                        <label for="address" class="text-dark-red mb-2">Indirizzo</label>
-                                        <input type="text" name="address" id="address"
-                                            class="form-control @error('address') is-invalid @enderror"
-                                            placeholder="Inserisci indirizzo" value="{{ old('address') }}" required>
-
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-                                        @error('address')
-                                            <small id="addressHlper" class="invalid-feedback">{{ $message }} </small>
-                                        @enderror
-
-                                    </div>
-
-                                    {{-- SEPARATORE --}}
-
-                                    <div class="row">
-                                        <label for="restaurant_image" class="text-dark-red mb-2">Immagine ristorante</label>
-                                        <input type="file"
-                                            class="form-control @error('restaurant_image') is-invalid @enderror"
-                                            id="restaurant_image" name="restaurant_image"
-                                            value="{{ old('restaurant_image') }}" required>
-
-                                        <small class="text_dark_red text-end">Campo obbligatorio</small>
-                                        @error('restaurant_image')
-                                            <small id="restaurant_imageHlper" class="invalid-feedback">{{ $message }}
-                                            </small>
-                                        @enderror
-                                    </div>
-
-
-
-                                    {{-- SEPARATORE --}}
-                                    <div class="row py-3 mb-0">
-                                        <div class="offset-md-4">
-                                            <button type="submit" class="btn_add rounded-5 fs-5 fw-bold px-4 py-1">Crea
-                                                ristorante</button>
-                                        </div>
-                                    </div>
-                                </form>
                             </div>
-    @endif
-@endsection
+
+                            {{-- SEPARATORE --}}
+
+                            <div class="row">
+                                <label for="phone" class="text-dark-red mb-2">Numero di telefono</label>
+                                <input type="text" name="phone" id="phone"
+                                    class="form-control @error('phone') is-invalid @enderror"
+                                    placeholder="Aggiungi numero di telefono" value="{{ old('phone') }}" required
+                                    minlength="9" maxlength="12">
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
+
+                                @error('phone')
+                                <small id="phoneHlper" class="invalid-feedback">{{ $message }} </small>
+                                @enderror
+
+                            </div>
+
+                            {{-- SEPARATORE --}}
+
+                            <div class="row">
+                                <label for="piva" class="text-dark-red mb-2">P.IVA</label>
+                                <input type="text" name="piva" id="piva"
+                                    class="form-control @error('piva') is-invalid @enderror" placeholder="Aggiungi piva"
+                                    value="{{ old('piva') }}" required minlength="11" maxlength="11">
+
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
+                                @error('piva')
+                                <small id="pivaHlper" class="invalid-feedback">{{ $message }} </small>
+                                @enderror
+
+                            </div>
+
+                            {{-- SEPARATORE --}}
+
+                            <div class="row">
+                                <label for="address" class="text-dark-red mb-2">Indirizzo</label>
+                                <input type="text" name="address" id="address"
+                                    class="form-control @error('address') is-invalid @enderror"
+                                    placeholder="Inserisci indirizzo" value="{{ old('address') }}" required>
+
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
+                                @error('address')
+                                <small id="addressHlper" class="invalid-feedback">{{ $message }} </small>
+                                @enderror
+
+                            </div>
+
+                            {{-- SEPARATORE --}}
+
+                            <div class="row">
+                                <label for="restaurant_image" class="text-dark-red mb-2">Immagine ristorante</label>
+                                <input type="file" class="form-control @error('restaurant_image') is-invalid @enderror"
+                                    id="restaurant_image" name="restaurant_image" value="{{ old('restaurant_image') }}"
+                                    required>
+
+                                <small class="text_dark_red text-end">Campo obbligatorio</small>
+                                @error('restaurant_image')
+                                <small id="restaurant_imageHlper" class="invalid-feedback">{{ $message }}
+                                </small>
+                                @enderror
+                            </div>
+
+
+
+                            {{-- SEPARATORE --}}
+                            <div class="row py-3 mb-0">
+                                <div class="offset-md-4">
+                                    <button type="submit" class="btn_add rounded-5 fs-5 fw-bold px-4 py-1">Crea
+                                        ristorante</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    @endif
+                    @endsection
+
+                    <style>
+                    .text_dark_red {
+                        color: #A43C28;
+                    }
+
+                    .dashboard_icon {
+                        width: 80px;
+                    }
+                    </style>
